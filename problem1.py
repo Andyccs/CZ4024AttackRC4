fd9a604ffe2e6007705ee5851f6f4a172b428a98
Andyccs <y4andy@gmail.com>
2016-02-28 22:51:03 +0800

    finish assignment

--- problem1.py
+++ problem1.py
@@ -1,4 +1,4 @@
-import binascii
+import copy
 
 PREFIX_LOGIN = 'LOGIN '
 PREFIX_MESSAGE = 'MESSAGE '
@@ -52,8 +52,7 @@ def main():
 
     # If server sends incorrect username message, then the client must have sent a login request
     # with incorrect username and password
-    plaintext = [cipher ^ message
-                  for cipher, message in zip(ciphers, BYTES_MESSAGE_INCORRECT_USERNAME)]
+    plaintext = xorByteArray(ciphers, BYTES_MESSAGE_INCORRECT_USERNAME)
 
     if startWithBytes(plaintext, BYTES_PREFIX_LOGIN):
       proxyPlaintext[index] = bytesToString(plaintext)
@@ -62,21 +61,78 @@ def main():
 
     # If server send password mismath, then the client must have sent a login request with
     # correct username but incorrect password
-    plaintext = [cipher ^ message
-                  for cipher, message in zip(ciphers, BYTES_MESSAGE_PASSWORD_MISMATCH)]
+    plaintext = xorByteArray(ciphers, BYTES_MESSAGE_PASSWORD_MISMATCH)
 
     if startWithBytes(plaintext, BYTES_PREFIX_LOGIN):
       proxyPlaintext[index] = bytesToString(plaintext)
       serverPlaintext[index] = MESSAGE_PASSWORD_MISMATCH
       continue
 
+    # If server send welcome, then the client must have sent a correct login request. 
+    # Get two characters every iteration
+    partialProxyPlaintext = xorByteArray(ciphers[0:len(BYTES_PREFIX_WELCOME)], BYTES_PREFIX_WELCOME)
+    if startWithBytes(partialProxyPlaintext, BYTES_PREFIX_LOGIN):
+      serverPlaintext[index], proxyPlaintext[index] = decryptIteratively(
+          ciphers, copy.deepcopy(BYTES_PREFIX_WELCOME), copy.deepcopy(BYTES_PREFIX_LOGIN), 1)
+      continue
+
+    partialProxyPlaintext = xorByteArray(ciphers[0:len(BYTES_PREFIX_REPLY_MESSAGE)],
+                                         BYTES_PREFIX_REPLY_MESSAGE)
+    if startWithBytes(partialProxyPlaintext, BYTES_PREFIX_MESSAGE):
+      serverPlaintext[index], proxyPlaintext[index] = decryptIteratively(
+          ciphers, copy.deepcopy(BYTES_PREFIX_REPLY_MESSAGE), copy.deepcopy(BYTES_PREFIX_MESSAGE),
+          3)
+      continue
+
   print 'Proxy Plaintext'
-  for t in proxyPlaintext:
-    print t
+  for index, text in enumerate(proxyPlaintext):
+    print index, text
 
   print 'Server Plaintext'
-  for t in serverPlaintext:
-    print t
+  for index, text in enumerate(serverPlaintext):
+    print index, text
+
+
+def decryptIteratively(ciphers, probableServerPlaintext, probablyProxyPlaintext, numSpace):
+  partialProxyPlaintext = xorByteArray(ciphers[0:len(probableServerPlaintext)],
+                                       probableServerPlaintext)
+  offset = len(probablyProxyPlaintext)
+  partialServerPlaintext = probableServerPlaintext
+  charactersPerIteration = len(probableServerPlaintext) - len(probablyProxyPlaintext)
+  index = 0
+  currentSpace = 0
+
+  while offset + (charactersPerIteration * index) + charactersPerIteration < LENGTH_OF_MESSAGE:
+    characters = ["" for i in range(charactersPerIteration)]
+    for j in range(charactersPerIteration):
+      characters[j] = partialProxyPlaintext[offset + (charactersPerIteration * index) + j]
+
+    for j in range(len(characters)):
+      character = characters[j]
+      if character == ord(' '):
+        currentSpace += 1
+        if currentSpace == numSpace:
+          characters = characters[0:j]
+          break
+
+    partialServerPlaintext.extend(characters)
+
+    partialProxyPlaintext = xorByteArray(ciphers[0:len(partialServerPlaintext)],
+                                         partialServerPlaintext)
+
+    if currentSpace == numSpace:
+      break
+
+    index += 1
+
+  serverPlaintext = addSpacesTillMaxLength(bytesToString(partialServerPlaintext))
+  proxyPlaintext = bytesToString(xorByteArray(ciphers, bytearray(serverPlaintext)))
+
+  return serverPlaintext, proxyPlaintext
+
+
+def xorByteArray(bytes1, bytes2):
+  return [b1 ^ b2 for b1, b2 in zip(bytes1, bytes2)]
 
 
 def bytesToString(bytes):
